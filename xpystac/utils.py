a183525d082226948e3675d7fa216fa4c809b7bd
Julia Signell <jsignell@gmail.com>
2023-09-06 09:58:41 -0400

    Guess engine for pystac-client ItemSearch objects (#28)
    It looks like checking the class name takes on the order of 6µs so seems
    like a better approach than importing.
    
    closes #27

--- xpystac/utils.py
+++ xpystac/utils.py
@@ -1,4 +1,5 @@
 import importlib
+from typing import Any
 
 
 def _import_optional_dependency(name):
@@ -7,3 +8,12 @@ def _import_optional_dependency(name):
     except ImportError as e:
         raise ImportError(f"Missing optional dependency '{name}'") from e
     return module
+
+
+def _is_item_search(obj: Any):
+    """Whether object is an instance of class pystac_client.ItemSearch.
+
+    Note: this function does not import pystac_client in the interest of
+    speed.
+    """
+    return obj.__class__.__name__ == "ItemSearch"
