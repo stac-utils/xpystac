342c1973157903b03eb8887954abf049a559b858
Julia Signell <jsignell@gmail.com>
2023-09-20 16:45:05 -0400

    Add odc-stac as an option in addition to stackstac (#26)

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,6 @@
+import dask.array
 import pytest
+import xarray as xr
 
 from xpystac.core import to_xarray
 
@@ -9,8 +11,26 @@ def test_to_xarray_with_cog_asset(simple_cog):
 
 
 def test_to_xarray_with_pystac_client_search(simple_search):
-    ds = to_xarray(simple_search, assets=["blue", "green", "red"])
+    ds = to_xarray(simple_search)
     assert ds
+
+
+def test_to_xarray_returns_dask_backed_object(simple_search):
+    ds = to_xarray(simple_search)
+    assert isinstance(ds.blue.data, dask.array.Array)
+    assert ds.blue.data.npartitions > 1
+
+
+def test_to_xarray_with_pystac_client_search_passes_kwargs_through(simple_search):
+    ds = to_xarray(simple_search, bands=["red", "green", "blue"], chunks={})
+    assert list(ds.data_vars) == ["red", "green", "blue"]
+    assert ds.blue.data.npartitions == 1
+
+
+@pytest.mark.parametrize("stacking_library", ["odc.stac", "stackstac"])
+def test_to_xarray_with_different_stacking_library(simple_search, stacking_library):
+    ds = to_xarray(simple_search, stacking_library=stacking_library)
+    assert isinstance(ds, xr.Dataset)
 
 
 def test_to_xarray_with_drop_variables_raises(simple_search):
