1f3607757a51c0f8e0ad1a68cda526d6af7795c9
Julia Signell <jsignell@gmail.com>
2025-03-11 15:57:54 +0100

    Update dev tooling (#42)
    Switch to uv, ruff and remove cassettes since they don't work with
    planetary computer signing

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,33 +7,28 @@ from tests.utils import STAC_URLS, requires_planetary_computer
 from xpystac.core import to_xarray
 
 
-@pytest.mark.vcr
 def test_to_xarray_with_cog_asset(simple_cog):
     ds = to_xarray(simple_cog)
     assert ds
 
 
-@pytest.mark.vcr
 def test_to_xarray_with_pystac_client_search(simple_search):
     ds = to_xarray(simple_search)
     assert ds
 
 
-@pytest.mark.vcr
 def test_to_xarray_returns_dask_backed_object(simple_search):
     ds = to_xarray(simple_search)
     assert isinstance(ds.blue.data, dask.array.Array)
     assert ds.blue.data.npartitions > 1
 
 
-@pytest.mark.vcr
 def test_to_xarray_with_pystac_client_search_passes_kwargs_through(simple_search):
     ds = to_xarray(simple_search, bands=["red", "green", "blue"], chunks={})
     assert list(ds.data_vars) == ["red", "green", "blue"]
     assert ds.blue.data.npartitions == 1
 
 
-@pytest.mark.vcr
 @pytest.mark.parametrize("stacking_library", ["odc.stac", "stackstac"])
 def test_to_xarray_with_different_stacking_library(simple_search, stacking_library):
     ds = to_xarray(simple_search, stacking_library=stacking_library)
@@ -42,7 +37,6 @@ def test_to_xarray_with_different_stacking_library(simple_search, stacking_libra
 
 
 @requires_planetary_computer
-@pytest.mark.vcr
 def test_to_xarray_with_pystac_client_search_with_patch_url():
     import planetary_computer as pc
     from rasterio.errors import RasterioIOError
@@ -68,20 +62,17 @@ def test_to_xarray_with_pystac_client_search_with_patch_url():
     assert ds.B01.max().compute() == 11080
 
 
-@pytest.mark.vcr
 def test_to_xarray_with_drop_variables_raises(simple_search):
     with pytest.raises(KeyError, match="not implemented for pystac items"):
         to_xarray(simple_search, drop_variables=["blue"])
 
 
-@pytest.mark.vcr
 def test_to_xarray_with_bad_type():
     with pytest.raises(TypeError):
         to_xarray("foo")
 
 
 @requires_planetary_computer
-@pytest.mark.vcr
 def test_to_xarray_reference_file():
     import planetary_computer as pc
     from fsspec.implementations.reference import ReferenceNotReachable
@@ -105,7 +96,6 @@ def test_to_xarray_reference_file():
 
 
 @requires_planetary_computer
-@pytest.mark.vcr
 def test_to_xarray_zarr():
     import planetary_computer as pc
 
@@ -123,7 +113,6 @@ def test_to_xarray_zarr():
 
 
 @requires_planetary_computer
-@pytest.mark.vcr
 def test_to_xarray_zarr_with_open_kwargs_engine():
     import planetary_computer as pc
 
