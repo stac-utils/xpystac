2a8b3c9b8e628d77b45ea3f18d2ed9436e5a0da1
Justus Magin <keewis@posteo.de>
2026-06-20 11:31:58 +0200

    don't interact with other extensions for now

--- xpystac/extensions.py
+++ xpystac/extensions.py
@@ -12,36 +12,16 @@ if TYPE_CHECKING:
 @dataclass
 class AssetInfo:
     href: str
-    additional_data: dict[str, JSON]
-    storage_refs: list[str] | None
-    auth_refs: list[str] | None
-
-    def resolve_storage(self, schemes: dict[str, JSON]) -> dict[str, JSON] | None:
-        refs = self.storage_refs
-        if refs is None:
-            return None
-
-        return {ref: schemes.get(ref) for ref in refs}
-
-    def resolve_auth(self, schemes: dict[str, JSON]) -> dict[str, JSON] | None:
-        refs = self.auth_refs
-        if refs is None:
-            return None
-
-        return {ref: schemes.get(ref) for ref in refs}
+    properties: dict[str, JSON]
 
 
 def _extract_alternate_asset(asset: pystac.Asset, alternate: str | None) -> AssetInfo:
-    excludes = {"href", "storage:refs", "auth:refs", "alternate:name"}
+    excludes = {"href", "alternate:name"}
 
     asset_name = asset.extra_fields.get("alternate:name")
     if alternate is None or alternate == asset_name:
         href = asset.href
-        storage_refs = asset.extra_fields.get("storage:refs")
-        auth_refs = asset.extra_fields.get("auth:refs")
-        additional_data = {
-            k: v for k, v in asset.extra_fields.items() if k not in excludes
-        }
+        properties = {k: v for k, v in asset.extra_fields.items() if k not in excludes}
     else:
         alternate_assets = asset.extra_fields.get("alternate")
         if alternate_assets is None:
@@ -57,12 +37,23 @@ def _extract_alternate_asset(asset: pystac.Asset, alternate: str | None) -> Asse
             )
 
         href = alternate_asset["href"]
-        storage_refs = alternate_asset.get("storage:refs")
-        auth_refs = alternate_asset.get("auth:refs")
-        additional_data = {
+        properties = {
             k: v
             for k, v in (asset.extra_fields | alternate_asset).items()
             if k not in excludes
         }
 
-    return AssetInfo(href, additional_data, storage_refs, auth_refs)
+    return AssetInfo(href, properties)
+
+
+def _resolve_scheme(
+    refs: list[str] | None, schemes: dict[str, dict[str, JSON]]
+) -> list[dict[str, JSON]] | None:
+    if refs is None:
+        return None
+
+    missing_refs = [ref for ref in refs if ref not in schemes]
+    if missing_refs:
+        raise ValueError("selected unknown refs: {', '.join(missing_refs)}")
+
+    return [schemes[ref] for ref in refs]
